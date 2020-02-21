d24ef1721aac7ef71668ee7fa85772099ccf7d37
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-02-21 14:42:27 +0100

    React to PR merges

--- github_bot/__main__.py
+++ github_bot/__main__.py
@@ -1,7 +1,12 @@
+import logging
+
 from octomachinery.app.server.runner import run as run_app
 from octomachinery.routing import process_event_actions
 from octomachinery.routing.decorators import process_webhook_payload
 from octomachinery.runtime.context import RUNTIME_CONTEXT
+
+
+logger = logging.getLogger(__name__)
 
 
 @process_event_actions('issues', {'opened'})
@@ -17,6 +22,31 @@ async def on_issue_opened(*, issue, **_kw):
         "I will look into it ASAP! (I'm a bot 🤖)."
     )
     await github_api.post(comments_api_url, data={"body": message})
+
+
+@process_event_actions('pull_request', {'closed'})
+@process_webhook_payload
+async def on_pr_merged(*, number, pull_request, **_kw):
+    """Whenever a PR is merged, say thanks to the author."""
+    github_api = RUNTIME_CONTEXT.app_installation_client
+
+    if not pull_request['merged']:
+        # interrupt early
+        logger.info('PR #%d just got closed but not merged', number)
+        return
+
+    logger.info('PR #%d just got merged', number)
+
+    comments_api_url = pull_request['comments_url']
+    author = pull_request['user']['login']
+    message = (
+        f'Thanks for the PR @{author}! '
+    )
+    await github_api.post(
+        comments_api_url, data={
+            'body': message,
+        },
+    )
 
 
 if __name__ == '__main__':
